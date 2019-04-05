68c24c6cb44505a3c8e3b2206c7b818a859aec5d
cle.ee@163.com <cle.ee@163.com>
2019-04-05 19:33:59 +0800

    word image parse tool ok

--- src/youdao/WordPictureExtractor.java
+++ src/youdao/WordPictureExtractor.java
@@ -1,53 +1,76 @@
 package youdao;
 
-import java.io.BufferedInputStream; 
-import java.io.BufferedReader; 
-import java.io.FileInputStream; 
-import java.io.InputStream; 
-import java.io.InputStreamReader; 
-import java.util.zip.ZipEntry; 
-import java.util.zip.ZipFile; 
-import java.util.zip.ZipInputStream; 
- 
- 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
 public class WordPictureExtractor {
- 
-  public static void main(String[] args) throws Exception {
-    try { 
-        readZipFile("C:\\Users\\i042416\\Pictures\\1.zip"); 
-      } catch (Exception e) { 
-        // TODO Auto-generated catch block 
-        e.printStackTrace(); 
-      } 
-  }
-  
-  public static void readZipFile(String file) throws Exception { 
-      ZipFile zf = new ZipFile(file); 
-      InputStream in = new BufferedInputStream(new FileInputStream(file)); 
-      ZipInputStream zin = new ZipInputStream(in); 
-      ZipEntry ze; 
-      while ((ze = zin.getNextEntry()) != null) { 
-        if (ze.isDirectory()) {
-        	continue;
-        }  
-        String fileName = ze.getName();
-        if( !fileName.contains("word/media/image"))
-        	continue;
-        System.out.println(fileName + " : " + ze.getSize() + " bytes"); 
-        long size = ze.getSize(); 
-        if (size > 0) { 
-            BufferedReader br = new BufferedReader( 
-                new InputStreamReader(zf.getInputStream(ze))); 
-            String line; 
-            while ((line = br.readLine()) != null) { 
-              // System.out.println(line); 
-            } 
-            br.close(); 
-          } 
-          System.out.println(); 
-      } 
-      zin.closeEntry(); 
-      zin.close();
-      zf.close();
-    } 
+
+	public final static String FOLDER = "C:\\Users\\i042416\\Pictures\\WORD\\";
+
+	public static void main(String[] args) throws Exception {
+		try {
+			readZipFile("C:\\Users\\i042416\\Pictures\\1.zip");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void copyImage(String fileName, InputStream input) {
+		try {
+			String[] result = fileName.split("/");
+			String outputFileName = FOLDER + result[2];
+			FileOutputStream fileOutputStream = new FileOutputStream(new File(
+					outputFileName));
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+			byte[] buffer = new byte[1024];
+			int length;
+
+			while ((length = input.read(buffer)) > 0) {
+				output.write(buffer, 0, length);
+			}
+
+			fileOutputStream.write(output.toByteArray());
+			input.close();
+			fileOutputStream.close();
+			System.out.println("File: " + outputFileName
+					+ " copied successfully!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void readZipFile(String file) throws Exception {
+		ZipFile zf = new ZipFile(file);
+		InputStream in = new BufferedInputStream(new FileInputStream(file));
+		ZipInputStream zin = new ZipInputStream(in);
+		ZipEntry ze;
+		while ((ze = zin.getNextEntry()) != null) {
+			if (ze.isDirectory()) {
+				continue;
+			}
+			String fileName = ze.getName();
+			if (!fileName.contains("word/media/image"))
+				continue;
+			System.out.println(fileName + " : " + ze.getSize() + " bytes");
+			long size = ze.getSize();
+			if (size > 0) {
+				InputStream image = zf.getInputStream(ze);
+				copyImage(fileName, image);
+			}
+		}
+		zin.closeEntry();
+		zin.close();
+		zf.close();
+	}
 }
